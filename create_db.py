dff352a84b979ac9e5daa0b7224d6bf3754af97b
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-06-13 10:17:36 +0300

    Updates

--- create_db.py
+++ create_db.py
@@ -1,14 +1,15 @@
 import sqlite3
 
+
 def create_db():
-    with open('hw.sql', 'r') as f:
+    with open("hw.sql", "r") as f:
         sql = f.read()
 
-
-    with sqlite3.connect('hw.db') as con:
+    with sqlite3.connect("hw.db") as con:
         cur = con.cursor()
 
         cur.executescript(sql)
+
 
 if __name__ == "__main__":
     create_db()
